4b293c90a45e7b9ebd0299970cc26900fe90ce44
Angela Z <100062360+angelazzzz@users.noreply.github.com>
2022-02-20 14:26:22 -0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,18 @@
+//home page
+
+function hideHome(){
+    if(document.getElementById("homeSection").style.display==="block"){
+    document.getElementById("homeSection").style.display="none";
+    } else {
+    document.getElementById("homeSection").style.display="block";
+    }
+}
+
+//triviaSection
+function hideTrivia(){
+    if(document.getElementById("triviaSection").style.display==="block"){
+    document.getElementById("triviaSection").style.display="none";
+    } else {
+    document.getElementById("triviaSection").style.display="block";
+    }
+}
